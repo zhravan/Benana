599e9139cd06835ee592745cd9e580bec6036e0b
zhravan <mrshravankumarb@gmail.com>
2025-08-31 04:08:59 +0530

    refactor(plugins): rename sample_powertable -> powertable and sample_reports -> reports; update names and responses

--- plugins/powertable/migrations/0001_init.sql
+++ plugins/powertable/migrations/0001_init.sql
@@ -1,4 +1,4 @@
--- sample powertable initial objects
+-- powertable initial objects
 CREATE TABLE IF NOT EXISTS powertable_items (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
---
+++ plugins/reports/__init__.py
@@ -0,0 +1,1 @@
+# reports plugin package
